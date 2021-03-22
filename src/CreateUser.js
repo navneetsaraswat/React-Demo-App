95378c8f7e91352198c33f1ab01edc13a3fa8fa5
Navneet Saraswat <navneets@chetu.com>
2021-03-22 18:35:54 +0530

    create user functionality

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import userEvent from '@testing-library/user-event';
+import React, { useState, useEffect } from 'react';
 import { Button, Navbar, Nav, FormControl, Form, Container, Row, Col } from 'react-bootstrap';
 function CreateUser() {
+
+    const [name, setName] = useState("");
+    const [salary, setSalary] = useState("");
+    const [age, setAge] = useState("");
+    const [message, setMessage] = useState("");
+
+    function CreateNewUser() {
+        let data = { name, salary, age };
+        console.warn("calling", data);
+        // useEffect(() => {
+            const dataObject =
+            {
+                method: 'POST',
+                Headers: { 'Contant-Type': 'application/json' },
+                body: JSON.stringify(data)
+            };
+            fetch("https://dummy.restapiexample.com/api/v1/create", dataObject).then((result)=>{
+                result.json().then(response=>{
+                    console.warn(response.data);
+                    setMessage("User created Successfully");
+                })
+            })
+        // }, []);
+
+    }
+
     return (
         <Container>
             <Row className="justify-content-md-center">
@@ -15,19 +42,19 @@ function CreateUser() {
                     <Form className="justify-content-md-center">
                         <Form.Group controlId="formGroupEmail">
                             <Form.Label>Name</Form.Label>
-                            <Form.Control type="text" placeholder="Name" />
+                            <Form.Control type="text" onChange={(e) => setName(e.target.value)} value={name} placeholder="Name" />
                         </Form.Group>
                         <Form.Group controlId="formGroupPassword">
                             <Form.Label>Salary</Form.Label>
-                            <Form.Control type="text" placeholder="Salary" />
+                            <Form.Control type="text" onChange={(e) => setSalary(e.target.value)} value={salary} placeholder="Salary" />
                         </Form.Group>
                         <Form.Group controlId="formGroupPassword">
                             <Form.Label>Age</Form.Label>
-                            <Form.Control type="text" placeholder="Age" />
+                            <Form.Control type="text" onChange={(e) => setAge(e.target.value)} value={age} placeholder="Age" />
                         </Form.Group>
-                     
-                        <Button variant="primary">Primary</Button>
+                        <Button variant="primary" onClick={CreateNewUser}>Primary</Button>
                     </Form>
+                    <span>{message}</span>
                 </Col>
             </Row>
         </Container>
